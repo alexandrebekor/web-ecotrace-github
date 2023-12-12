226c42ccdef4fa275d8f5e220500cb1caf9c8aef
Alexandre Bekor <alexandre@agenciabekor.com>
2023-12-12 14:54:21 -0300

    fix workflow test e2e

--- apps/api/src/lib/env.ts
+++ apps/api/src/lib/env.ts
@@ -2,9 +2,7 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const schema = z.object({
-	HOST: z.string(),
 	PORT: z.coerce.number().default(3333),
-	HOST_URL: z.string(),
 	NODE_ENV: z.enum(['development', 'production', 'test']),
 	TOKEN_GITHUB: z.string(),
 	DATABASE_URL: z.string(),
